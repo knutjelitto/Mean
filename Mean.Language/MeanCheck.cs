52278e8a00d3b08ab36782f56094623363db1a20
Jelitto, Knut <knut.jelitto@sartorius.com>
2019-08-22 11:58:21 +0200

    setup new project

---
+++ Mean.Language/MeanCheck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Hime.Redist;
+
+namespace Mean.Language
+{
+    public static class MeanCheck
+    {
+        public static void Run()
+        {
+            var lexer = new MeanLexer("2 + 3 * 4");
+            var parser = new MeanParser(lexer);
+
+            var result = parser.Parse();
+
+            Print(result.Root, new bool[] { });
+        }
+
+        private static void Print(ASTNode node, bool[] crossings)
+        {
+            for (var i = 0; i < crossings.Length - 1; i++)
+            {
+                Console.Write(crossings[i] ? "|   " : "    ");
+            }
+            if (crossings.Length > 0)
+            {
+                Console.Write("+-> ");
+            }
+            Console.WriteLine(node.ToString());
+
+            for (var i = 0; i != node.Children.Count; i++)
+            {
+                var childCrossings = new bool[crossings.Length + 1];
+                Array.Copy(crossings, childCrossings, crossings.Length);
+                childCrossings[childCrossings.Length - 1] = (i < node.Children.Count - 1);
+                Print(node.Children[i], childCrossings);
+            }
+        }
+    }
+}
---
+++ Mean.Runner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mean.Language;
+
+namespace Mean.Runner
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            MeanCheck.Run();
+
+            Console.ReadKey(true);
+        }
+    }
+}
